568b737033942253b9b317e518dca1ad98acd168
taffy203 <129981163+taffy203@users.noreply.github.com>
2023-04-23 12:39:28 +0100

    added current Icon to the weather app

--- index.js
+++ index.js
@@ -30,21 +30,26 @@ dateElement.innerHTML = `${createDate(
 )} ${currentHour}:${currentMinutes}`;
 
 function displayTemperature(response) {
-  console.log(response);
   let currentTemperature = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
+
   currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
 
 let apiKey = "40f1f0763f9od6a15t0ca82fb440c4ef";
-let city = "London";
+let city = "Nottingham";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
