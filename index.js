d96e13a946a4dc7b3f33f9e397ce8d48119ab75f
taffy203 <129981163+taffy203@users.noreply.github.com>
2023-04-23 10:34:44 +0100

    fixed the problem and added current humidity and wind

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function displayTemperature(response) {
-  let currentTemperature = document.querySelector("#temperatue");
+  let currentTemperature = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   currentTemperature.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "40f1f0763f9od6a15t0ca82fb440c4ef";
